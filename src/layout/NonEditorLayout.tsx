75dbcca296efdf5a2ca5e9ebbfbaf4d6c2ce0833
Phil <phil@upvalue.io>
2025-10-09 11:10:05 -0700

    use cn for NonEditorLayout (styling issue)

--- src/layout/NonEditorLayout.tsx
+++ src/layout/NonEditorLayout.tsx
@@ -1,11 +1,12 @@
 import { GUTTER_WIDTH_PIXELS } from "@/editor/constants"
+import { cn } from "@/lib/utils"
 
 /**
 * Pads a non editor page (like the not found document) to look like te editor
 */
 const NonEditorLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className={`flex flex-col h-full space-y-4 px-[${GUTTER_WIDTH_PIXELS}px] py-4`}>
+        <div className={cn(`flex flex-col h-full space-y-4 px-[${GUTTER_WIDTH_PIXELS}px] py-4`)}>
             {children}
         </div>
     )
